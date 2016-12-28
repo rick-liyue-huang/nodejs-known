7b68c3318a74752423ff050fe8ae6e71e5d89bba
rickliyuehuang <rick.liyue.huang@gmail.com>
2016-12-28 22:32:40 +1100

    modify nodepath

--- 01_nodepath/1_simple_intro/01_helloworld.js
+++ 01_nodepath/1_simple_intro/01_helloworld.js
@@ -43,17 +43,13 @@
 // the console.log is the most common use in terminal and also the most
 // important debug tool in mode.js
 
+
 "use strict";
 
 setTimeout(() => {
 
 	console.log('hello world');
 }, 2000);
-
-
-
-
-
 
 
 
