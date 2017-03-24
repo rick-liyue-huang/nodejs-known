5fcf328e4cc3cd0d89f58c6dfeb8685f194907cb
rickliyuehuang <rick.liyue.huang@gmail.com>
2017-03-25 00:42:08 +1100

    modify stream

--- 01_nodepath/6/01_stream.js
+++ 01_nodepath/6/01_stream.js
@@ -4,77 +4,32 @@
 const fs = require('fs');
 const path = require('path');
 
-let sourcePath = 'D:/Softwares/OS/ubuntu-15.04-desktop-amd64.iso';
-let distPath = 'C:/Users/rick/Desktop/ubuntu-15.04-desktop-amd64.iso';
+let sourcePath = '/users/leo/downloads/day1.zip';
+let distPath = '/users/leo/desktop/day1.zip';
 
-
-// get the file size
 let totalSize = fs.statSync(sourcePath).size;
+let curSize = 0;
 
-// 1. create a readstream
+//1.create a readStream
 let rs = fs.createReadStream(sourcePath);
 
-// 2. create a writeStream
+//2. create a writeStream
 let ws = fs.createWriteStream(distPath);
 
-var surSize = 0;
-
-// 3. watch the readstream data event, if the 'data' event is triggered, we will put the 
-// first argument of the callback func 'chunk' to the curSize
+//3.listen the readStream data event, if the event is triggered, we will transfer the first parameters of the callback to writeStream method
+// chunk is the data block, so it is the Buffer object
 
 rs.on('data', (chunk) => {
 	curSize += chunk.length;
-
 	let percentage = (curSize / totalSize * 100).toFixed(2);
-
-	console.log(percentage);
-// 4. and we also add the chunk into the ws
+	console.log(`the percentage is: ${percentage} %`);
 	ws.write(chunk);
 });
 
-
-// 5. we do not know what time the rs will end, but we can watch the 'end' event of rs, 
-rs.on('end', () => {
-
-	// 6. when we watched the 'end' event, we 'close' ws.
-	ws.close();
+//4. we do not know what time the writeStream operation finished, so we use end event to listen.
+rs.on('end', (err) => {
+	if (err) throw err;
+	console.log('successful');
 });
 
-
-// 7. we also can replace the upon lines by just one line code in pipe func
-
-// rs.pipe(ws);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+rs.pipe(ws);
