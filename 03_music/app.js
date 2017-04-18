ce7442a98a81a4be8fe0f6ed2a6f8f71588cb877
rickliyuehuang <rick.liyue.huang@gmail.com>
2017-04-19 02:08:40 +1000

    add edit

--- 03_music/app.js
+++ 03_music/app.js
@@ -51,8 +51,10 @@
 	const fs = require(`fs`);
 	const path = require(`path`);
 	const querystring = require(`querystring`); 
+	const url = require(`url`);
 	const config = require(`./config.js`);
 	const mime = require(`./mime.json`);
+	const artTemplate = require(`art-template`);
 
 
 	// create http server
@@ -65,10 +67,18 @@
 
 		res.json = responseJson(res);
 
+		res.redirect = redirect(res);
+
 		// res.end(`hello world`);
 
 		// get the req path
-		let url = decodeURI(req.url);
+		let urlObj = url.parse(req.url, true);
+
+		// 为了后面的代码方便操作，我们约定好，把所有的查询字符串转换为 对象之间
+		// 挂载到 request对象上的query属性上
+		req.query = urlObj.query;
+
+		let pathname = urlObj.pathname;
 		let method = req.method;
 
 	 /* static source
@@ -80,7 +90,7 @@
 
 		*/ 
 
-		if (method === `GET` && url === `/`) {
+		if (method === `GET` && pathname === `/`) {
 			/*
 			fs.readFile(path.join(__dirname, `index.html`), (err, data) => {
 				if (err) {
@@ -94,10 +104,10 @@
 
 			res.render(`index`);
 
-		} else if (method === `GET` && url.startsWith(`/node_modules/`)) {
+		} else if (method === `GET` && pathname.startsWith(`/node_modules/`)) {
 
 			// get the current static source absolute path
-			let fullPath = path.join(__dirname, url);
+			let fullPath = path.join(__dirname, pathname);
 			console.log(fullPath);
 
 			fs.readFile(fullPath, (err, data) => {
@@ -110,7 +120,7 @@
 			});
 
 
-		} else if (method === `GET` && url === `/music`) {
+		} else if (method === `GET` && pathname === `/music`) {
 
 			//  先构造要相应给客户端的json对象
 			let send = {
@@ -128,7 +138,7 @@
 			res.end(sendBuf); // res.end()内部可以自动将字符串转换为二进制
 
 
-		} else if (method === `GET` && url.startsWith(`/files/`)) {
+		} else if (method === `GET` && pathname.startsWith(`/files/`)) {
 
 
 			// 对于下面的写法来说，mp3文件很大，这种做法是一次性的读取出来，然后相应到客户端
@@ -140,7 +150,7 @@
 			// 	res.end(data);
 			// });
 
-			let fullPath = path.join(__dirname, url);
+			let fullPath = path.join(__dirname, pathname);
 
 			// 手动的方式创建一个读取流
 			let readStream = fs.createReadStream(fullPath);
@@ -148,7 +158,7 @@
 			// 我们通过调用读取流的pipe
 			readStream.pipe(res);
 
-		} else if (method === `GET` && url === `/add`) {
+		} else if (method === `GET` && pathname === `/add`) {
 
            /*
 			fs.readFile(path.join(__dirname, `add.html`), (err, data) => {
@@ -161,7 +171,7 @@
 			*/
 			res.render(`add`);
 
-		} else if (method === `POST` && url === `/add`) {
+		} else if (method === `POST` && pathname === `/add`) {
 
 			let data = ``;
 			// 接受post方式提交的数据
@@ -191,16 +201,43 @@
 
 				// 代码到这里表示可以添加歌曲
 				musicList.push(obj);
+				
+
+				// 重新定向网页地址
+				res.redirect(`http://127.0.0.1:3000/`);
+
 				res.json({
 					code: `5000`,
 					msg: `successfully add music`
 				});
 			});
+
+		} else if (method === `GET` && pathname === `/edit`) {
+
+			// 从查询字符串中获取用户要编辑的歌曲信息 id
+		    let mid = req.query.mid;
+
+		    // 根据歌曲id，找到数据中的该项
+		    let music = musicList.find(m => m.id === mid);
+
+		    if (!music) {
+		      return res.json({
+		        code:'5003',
+		        msg:'music not found'
+		      });
+		    }
+
+		    // 在 es6 中，可以
+		    res.render('edit',{
+		      music: music
+		    });
+
 		}
 	});
 
 
 //  当调用了render方法之后，render方法就返回一个新的函数，这是高阶函数-- 函数内部返回新的函数
+/*
 	function render(res) {
 
 		return function(filename) {
@@ -215,6 +252,19 @@
 		};
 		
 	}
+	*/
+
+	function render(res) {
+	  return function(fileName, data) {
+	    // 对于有的页面不需要注入数据，所以我们写了一个短路的 data || {}
+	    // 目的是为了出现 undefined
+	    let htmlStr = artTemplate(`${__dirname}/${fileName}`, data || {});
+	    res.writeHead(200, {
+	      'Content-Type': 'text/html; charset=utf-8'
+	    });
+	    res.end(htmlStr);
+	  };
+	}
 
 	function responseJson(res) {
 
@@ -222,6 +272,15 @@
 			let jsonStr = JSON.stringify(jsonObj);
 			res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
 			res.end(jsonStr);
+		};
+	}
+
+	function redirect(res) {
+		return function(path) {
+			res.writeHead(302, {
+				"Location": path
+			});
+			res.end();
 		};
 	}
 
